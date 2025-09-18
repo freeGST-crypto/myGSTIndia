f1483d583462d76524c25f051a775daafcc156b2
Gharkakhana <gharkakhana0958@gmail.com>
2025-09-18 08:46:12 +0000

    Try fixing this error: `Runtime Error: Error: Textarea is not defined. E

--- src/app/(app)/ca-certificates/net-worth/page.tsx
+++ src/app/(app)/ca-certificates/net-worth/page.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import { useToast } from "@/hooks/use-toast";
 import { useReactToPrint } from "react-to-print";
 import { Table, TableBody, TableCell, TableFooter as TableFoot, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Textarea } from "@/components/ui/textarea";
 
 
 const assetSchema = z.object({
@@ -304,5 +305,3 @@ export default function NetWorthCertificatePage() {
     </div>
   );
 }
-
-    
